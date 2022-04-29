906dec65424224fc99648fca1335e4c76e3a31b8
Pranav Vashi <neobuddy89@gmail.com>
2022-04-30 02:40:17 +0530

    Settings: Also hide pill length on taskbar mode
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
+++ src/com/android/settings/gestures/GestureNavigationSettingsFragment.java
@@ -55,6 +55,7 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
     private static final String GESTURE_BACK_HEIGHT_KEY = "gesture_back_height";
     private static final String GESTURE_NAVBAR_RADIUS_KEY = "gesture_navbar_radius_preference";
     private static final String NAVIGATION_BAR_HINT_KEY = "navigation_bar_hint";
+    private static final String NAVIGATION_BAR_LENGTH_KEY = "gesture_navbar_length_preference";
 
     private WindowManager mWindowManager;
     private BackGestureIndicatorView mIndicatorView;
@@ -102,6 +103,8 @@ public class GestureNavigationSettingsFragment extends DashboardFragment {
         boolean isTaskbarEnabled = LineageSettings.System.getInt(getContext().getContentResolver(),
                 LineageSettings.System.ENABLE_TASKBAR, isLargeScreen(getContext()) ? 1 : 0) == 1;
         if (isTaskbarEnabled) {
+            getPreferenceScreen().removePreference(
+                    getPreferenceScreen().findPreference(NAVIGATION_BAR_LENGTH_KEY));
             getPreferenceScreen().removePreference(
                     getPreferenceScreen().findPreference(NAVIGATION_BAR_HINT_KEY));
         }
